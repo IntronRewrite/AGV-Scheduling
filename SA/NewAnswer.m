0d9c4ac401d17b0af45ee2dc9c244afa1a237f6a
IntronRewrite <129854778+IntronRewrite@users.noreply.github.com>
2024-06-04 20:05:26 +0800

    Update NewAnswer.m

--- SA/NewAnswer.m
+++ SA/NewAnswer.m
@@ -1,11 +1,19 @@
-function X=NewAnswer(S1)
-%% 输入
-% S1:当前解
-%% 输出
-% X：新解
-N=length(S1);
-X=S1;                
-a=round(rand(1,2)*(N-1)+1); %产生两个随机位置 用来交换
-W=X(a(1));
-X(a(1))=X(a(2));
-X(a(2))=W;         %得到一个新路线
+function X = NewAnswer(S1)
+% Generate a new solution by swapping two random positions in the current solution
+% Inputs:
+% S1 - Current solution
+% Outputs:
+% X - New solution
+
+N = length(S1); % Length of the current solution
+X = S1; % Initialize the new solution with the current solution
+
+% Generate two random positions to swap
+a = round(rand(1, 2) * (N - 1) + 1);
+
+% Swap the elements at the two positions
+W = X(a(1));
+X(a(1)) = X(a(2));
+X(a(2)) = W;
+
+end
