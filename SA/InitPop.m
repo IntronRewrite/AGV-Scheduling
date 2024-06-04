33d9665f447206ac0d48f626ad449aa93852dcb7
IntronRewrite <129854778+IntronRewrite@users.noreply.github.com>
2024-06-04 20:04:11 +0800

    Update InitPop.m

--- SA/InitPop.m
+++ SA/InitPop.m
@@ -1,22 +1,32 @@
-function Chrom = InitPop(X,Y,m,pop)
-%%初始化种群 根据左时间窗插入
-% 输入
-% X 卸货区
-% Y 装货区
-% m 小车数目
-% pop 种群数目
+function Chrom = InitPop(X, Y, m, pop)
+% Initialize the population based on left time windows
+% Inputs:
+% X - Unloading area
+% Y - Loading area
+% m - Number of AGVs (Automated Guided Vehicles)
+% pop - Population size
+% Outputs:
+% Chrom - Initial population solutions
 
-% 输出
-% Chrom 种群初始解
+% Combine task points and sort by the left time window
+Task = [X; Y];
+Task = sortrows(Task, 2);
+Task = Task(:, 1)';
 
-Task=[X;Y];%任务点合并，按左时间窗排序
-Task=sortrows(Task,2);
-Task = Task(:,1)';
-xlength = size(X,1); %卸箱个数
-ylength = size(Y,1); %装箱个数
-len = xlength + ylength + m - 1; %染色体长度
-Chrom = zeros(pop,len); %初始化种群
-cars = xlength + ylength + 1: xlength + ylength + m - 1;%AGV小车编号
+% Get the number of unloading and loading points
+xlength = size(X, 1); % Number of unloading points
+ylength = size(Y, 1); % Number of loading points
+len = xlength + ylength + m - 1; % Length of chromosome
+
+% Initialize the population
+Chrom = zeros(pop, len);
+
+% Generate AGV identifiers
+cars = xlength + ylength + 1 : xlength + ylength + m - 1;
+
+% Generate initial population
 for i = 1:pop
-    Chrom(i,:) = [Task cars];%生成初始种群
+    Chrom(i, :) = [Task, cars]; % Combine task points and AGV identifiers
+end
+
 end
