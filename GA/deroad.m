557d40abe9112257eaf989009711ab5289181039
IntronRewrite <129854778+IntronRewrite@users.noreply.github.com>
2024-06-04 19:32:53 +0800

    Update deroad.m

--- GA/deroad.m
+++ GA/deroad.m
@@ -1,28 +1,34 @@
-function caroads = deroad(Chrom,lengthx,lengthy,rowcar)
-%%染色体分解小车路径
-%输入
-%Chrom 染色体
-%lengthx 卸货区个数
-%lengthy 装货区个数
-%rowcar 小车数目
-%输出
-%caroads 小车运行路径
-carindex = find(Chrom>lengthx+lengthy);%找到小车在染色体中的位置
-caroads = cell(rowcar,1);%小车路径
-for j = 1:rowcar%分解每个小车的路径
-    if j==1 || j==rowcar
-        if j==1
-            thefirst = 1;
-            thelast = carindex(j)-1;
-        else
-            thefirst = carindex(j-1)+1;
-            thelast = lengthx + lengthy + rowcar - 1;
-        end
+function caroads = deroad(Chrom, lengthx, lengthy, rowcar)
+% Decompose chromosome into individual AGV paths
+% Inputs:
+% Chrom - Chromosome
+% lengthx - Number of unloading areas
+% lengthy - Number of loading areas
+% rowcar - Number of AGVs
+% Outputs:
+% caroads - AGV paths
+
+% Find the positions of the AGVs in the chromosome
+carindex = find(Chrom > lengthx + lengthy);
+% Initialize the AGV paths
+caroads = cell(rowcar, 1);
+
+% Decompose each AGV's path
+for j = 1:rowcar
+    if j == 1
+        thefirst = 1;
+        thelast = carindex(j) - 1;
+    elseif j == rowcar
+        thefirst = carindex(j - 1) + 1;
+        thelast = lengthx + lengthy + rowcar - 1;
     else
-        thefirst = carindex(j-1)+1;
-        thelast = carindex(j)-1;
+        thefirst = carindex(j - 1) + 1;
+        thelast = carindex(j) - 1;
     end
-    if thefirst<=thelast
-        caroads{j}=Chrom(thefirst:thelast);
+    
+    if thefirst <= thelast
+        caroads{j} = Chrom(thefirst:thelast);
     end
+end
+
 end
