6512451c5f3f680f325c9d53f21fed15311f7751
IntronRewrite <129854778+IntronRewrite@users.noreply.github.com>
2024-06-04 20:01:53 +0800

    Update Fitness.m

--- SA/Fitness.m
+++ SA/Fitness.m
@@ -1,35 +1,54 @@
-function Fit = Fitness(Chrom,Pos,X,Y,Map,Tlast,pf,pw,pql,pqr,py,tload,v)
-%%计算种群适应度 采用惩罚函数
-%输入
-%Chrom 种群
-%Pos 坐标点
-%X 卸箱区及时间窗
-%Y 装箱区及时间窗
-%Map 映射函数
-%Tlast 小车最晚时间
-%pa "重进重出"惩罚因子
-%pb 岸桥惩罚因子
-%pload 场桥惩罚因子
-%tload 装载时间
-%v AGV行驶速度
-%输出
-%Fit 种群适应度
-rowcar = size(Tlast,1);%agv个数
-rowchrom = size(Chrom,1);%染色体数目
-lengthx = size(X,1);%卸箱个数
-lengthy = size(Y,1);%装箱个数
-Fit = zeros(rowchrom,7);%适应度值初始化
+function Fit = Fitness(Chrom, Pos, X, Y, Map, Tlast, pf, pw, pql, pqr, py, tload, v)
+% Calculate the fitness of the population using penalty functions
+% Inputs:
+% Chrom - Population (chromosomes)
+% Pos - Coordinate points
+% X - Unloading area and time window
+% Y - Loading area and time window
+% Map - Mapping function
+% Tlast - Latest time for AGVs
+% pf - Penalty factor for "unload before load"
+% pw - Penalty factor for overweight
+% pql - Penalty factor for earliest crane
+% pqr - Penalty factor for latest crane
+% py - Penalty factor for yard crane
+% tload - Loading time
+% v - AGV travel speed
+% Outputs:
+% Fit - Fitness values of the population
+
+% Get the number of AGVs
+rowcar = size(Tlast, 1);
+% Get the number of chromosomes
+rowchrom = size(Chrom, 1);
+% Get the number of unloading points
+lengthx = size(X, 1);
+% Get the number of loading points
+lengthy = size(Y, 1);
+% Initialize fitness values
+Fit = zeros(rowchrom, 7);
 
 for i = 1:rowchrom
-    caroads = deroad(Chrom(i,:),lengthx,lengthy,rowcar);%编码分解出各小车路径
-    [runtime,wrongtime,wrongcountyl,wrongcountyr,wrongcountql,wrongcountqr] = costime(caroads,X,Y,Map,Tlast,Pos,tload,v,pql,pqr,py);%计算个体车辆运行时间 时间窗惩罚值 不满足岸桥时间窗个数 不满足场桥时间窗个数 
-    unlilo = unloadinloadout(caroads,X,Y);%计算不合符重进重出的情况
-    overweight= overwcount(caroads,X,Y,Tlast);
-    Fit(i,1) = runtime+wrongtime+unlilo*pf+overweight*pw;
-    Fit(i,2) = wrongcountyr;
-    Fit(i,3) = wrongcountqr;
-    Fit(i,4) = wrongcountyl;
-    Fit(i,5) = wrongcountql;
-    Fit(i,6) = unlilo;
-    Fit(i,7) = overweight;
+    % Decode the chromosome to obtain each AGV path
+    caroads = deroad(Chrom(i, :), lengthx, lengthy, rowcar);
+    
+    % Calculate individual vehicle runtime, time window penalties,
+    % the number of violations for crane and yard crane time windows
+    [runtime, wrongtime, wrongcountyl, wrongcountyr, wrongcountql, wrongcountqr] = costime(caroads, X, Y, Map, Tlast, Pos, tload, v, pql, pqr, py);
+    
+    % Calculate the number of violations for "unload before load"
+    unlilo = unloadinloadout(caroads, X, Y);
+    
+    % Calculate the overweight count
+    overweight = overwcount(caroads, X, Y, Tlast);
+    
+    % Calculate the fitness value considering penalties
+    Fit(i, 1) = runtime + wrongtime + unlilo * pf + overweight * pw;
+    Fit(i, 2) = wrongcountyr;
+    Fit(i, 3) = wrongcountqr;
+    Fit(i, 4) = wrongcountyl;
+    Fit(i, 5) = wrongcountql;
+    Fit(i, 6) = unlilo;
+    Fit(i, 7) = overweight;
+end
 end
